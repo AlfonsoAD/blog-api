c1c36dfd314b72dbb10afe0e653e5878ef35adfe
AlfonsoAD <alfonsoad097@gmail.com>
2023-10-12 19:55:53 -0500

    wsgi

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+import sys
+sys.path.append('/home/django_projects/blog')
+sys.path.append('/home/django_projects/blog/blog')
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
